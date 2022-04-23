1fb4496b40ce77e510c3d2b1d9c6b937dc197c94
Artikon90 <artikon90@gmail.com>
2022-04-23 21:22:28 +0300

    Использую уточнение через @Qualifier

--- src/main/java/classes/MusicPlayer.java
+++ src/main/java/classes/MusicPlayer.java
@@ -1,12 +1,16 @@
 package classes;
 
+import interfaces.Music;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 @Component
 public class MusicPlayer {
-    private RockMusic music1;
-    private ClassicalMusic music2;
+    private Music music1;
+    private Music music2;
 
-    public MusicPlayer(RockMusic music1, ClassicalMusic music2) {
+@Autowired
+    public MusicPlayer(@Qualifier("rockMusic") Music music1, @Qualifier("classicalMusic") Music music2) {
         this.music1 = music1;
         this.music2 = music2;
     }
